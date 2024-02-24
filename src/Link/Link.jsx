9c6479bcdb8f0f64f5acbd51ea0d61eb3973ec3f
Perevalov Anton <mormolad@yandex.ru>
2024-02-24 19:15:54 +0700

    Jest настроен, подружил его с lint-stage

--- src/Link/Link.jsx
+++ src/Link/Link.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react';
 
 const STATUS = {
-	HOVERED: 'hovered',
-	NORMAL: 'normal',
+  HOVERED: 'hovered',
+  NORMAL: 'normal',
 };
 
 export default function Link({ page, title }) {
-	const [status, setStatus] = useState(STATUS.NORMAL);
+  const [status, setStatus] = useState(STATUS.NORMAL);
 
-	const onMouseEnter = () => {
-		setStatus(STATUS.HOVERED);
-	};
+  const onMouseEnter = () => {
+    setStatus(STATUS.HOVERED);
+  };
 
-	const onMouseLeave = () => {
-		setStatus(STATUS.NORMAL);
-	};
+  const onMouseLeave = () => {
+    setStatus(STATUS.NORMAL);
+  };
 
-	return (
+  return (
 		<a
 			className={status}
 			href={page || '#'}
@@ -25,5 +25,5 @@ export default function Link({ page, title }) {
 		>
 			{title}
 		</a>
-	);
+  );
 }
