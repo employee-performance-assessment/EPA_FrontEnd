c7973fc5d5fab3e3acb15095675ddb0a0cbdb3ca
Perevalov Anton <mormolad@yandex.ru>
2024-02-24 16:05:26 +0700

    fsdsdfd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 // import React from 'react';
+import Link from './Link/Link';
 
 function App() {
-	return <div />;
+	return (
+		<div>
+			<Link title={'learn react'} />
+		</div>
+	);
 }
 
 export default App;
