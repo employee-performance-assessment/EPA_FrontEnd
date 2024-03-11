5a45350a2f9512e40de13cb4a2c5df8c7fcd252b
Irina Garmaeva <garmaeva-i@mail.ru>
2024-03-11 20:22:59 +0100

    feat(checkbox): add Checkbox component

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Checkbox.scss';
+
+const Checkbox = ({ label1, label2 }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <label className={`switch-checkbox ${isChecked ? 'checked' : ''}`} onClick={handleToggle}>
+      <div className="left-side">
+        <span className=''>{label1}</span>
+      </div>
+      <div className="right-side">
+        <span cl>{label2}</span>
+      </div>
+    </label>
+  );
+};
+
+export default Checkbox;
