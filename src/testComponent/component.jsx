b902260f39c20255fa38aebe66526fb77ae87507
mormolad <paspean@mail.ru>
2024-03-11 21:21:33 +0700

    feat: config lint

--- src/testComponent/component.jsx
+++ src/testComponent/component.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import selectUserModule from '../services/redux/user/selectors';
 
 function TestComponent() {
-	const { role, firstName, lastName } = useSelector(selectUserModule);
-	return (
-		<div>
-			<div>Привет, {`${firstName} ${lastName}` || 'user'}!</div>
-			{role === 'admin' && <div>Ты админ :)</div>}
-		</div>
-	);
+  const { role, firstName, lastName } = useSelector(selectUserModule);
+  return (
+    <div>
+      <div>Привет, {`${firstName} ${lastName}` || 'user'}!</div>
+      {role === 'admin' && <div>Ты админ :)</div>}
+    </div>
+  );
 }
 
 export default TestComponent;
