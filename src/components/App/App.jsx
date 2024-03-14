b28e0742011f75636857ca403d9fc92500a38ae0
Дмитрий Русинов <dmitry-rusinov123@yandex.ru>
2024-03-15 01:01:29 +0300

    create Login and Register pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.jsx';
 import NotFound from '../NotFound/NotFound.jsx';
