2f7e57153e3e271da8afcf95f457319a925bf1ad
Irina Garmaeva <garmaeva-i@mail.ru>
2024-03-24 19:03:29 +0100

    refactor(checkbox): refactor Checkbox component
    move state, setState function to props, add shadow prop

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,26 +1,18 @@
-import { useState } from 'react';
 import './Checkbox.scss';
 
-const Checkbox = ({ labelLeft, labelRight }) => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleToggle = () => {
-    setIsChecked(!isChecked);
-  };
-
-  return (
-    <label
-      className={`switch-checkbox ${isChecked ? 'checked' : ''}`}
-      onClick={handleToggle}
-    >
-      <div className="left-side">
-        <span>{labelLeft}</span>
-      </div>
-      <div className="right-side">
-        <span>{labelRight}</span>
-      </div>
-    </label>
-  );
+const Checkbox = ({ labelLeft, labelRight, isChecked, setIsChecked, shadow }) => {
+  <label
+    style={{ boxShadow: shadow }}
+    className={`switch-checkbox ${isChecked ? 'checked' : ''}`}
+    onClick={() => setIsChecked(!isChecked)}
+  >
+    <div className="left-side">
+      <span>{labelLeft}</span>
+    </div>
+    <div className="right-side">
+      <span>{labelRight}</span>
+    </div>
+  </label>;
 };
 
 export default Checkbox;
