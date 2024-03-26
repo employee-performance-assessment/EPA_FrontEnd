ee2c682dc0548abd2c04452ba29086bb1aeb7b28
Olga R <pepsitrash@mail.ru>
2024-03-27 00:27:21 +0300

    feat: solve conflicts, rearrange file structure for EmployeeView components

--- src/pages/EmployeeViewPage/EmployeeViewPage.jsx
+++ src/pages/EmployeeViewPage/EmployeeViewPage.jsx
@@ -1,9 +1,9 @@
 import styles from './EmployeeViewPage.module.scss';
-import SideMenu from '../SideMenu/SideMenu.jsx';
-import EmployeeViewHeader from '../EmployeeViewHeader/EmployeeViewHeader.jsx';
-import Checkbox from '../Checkbox/Checkbox.jsx';
-import EmployeeViewFilter from '../EmployeeViewFilter/EmployeeViewFilter.jsx';
-import EmployeeViewBlock from '../EmployeeViewBlock/EmployeeViewBlock.jsx';
+import SideMenu from '../../components/SideMenu/SideMenu.jsx';
+import EmployeeViewHeader from '../../components/EmployeeViewHeader/EmployeeViewHeader.jsx';
+import Checkbox from '../../components/Checkbox/Checkbox.jsx';
+import EmployeeViewFilter from '../../components/EmployeeViewFilter/EmployeeViewFilter.jsx';
+import EmployeeViewBlock from '../../components/EmployeeViewBlock/EmployeeViewBlock.jsx';
 
 function EmployeeViewPage() {
   return (
