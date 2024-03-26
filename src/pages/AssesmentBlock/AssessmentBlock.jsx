676aeacab772764e7ae48713d872a5e7814a6156
Darya Kolotilina <kolotilina.d@gmail.com>
2024-03-26 20:02:48 +0200

    'feat: create first page'

--- src/pages/AssesmentBlock/AssessmentBlock.jsx
+++ src/pages/AssesmentBlock/AssessmentBlock.jsx
@@ -1,6 +1,7 @@
 import SideMenu from '../../components/SideMenu/SideMenu.jsx';
 import './AssessmentBlock.scss';
 import icon from '../../images/assessmentBlock_icon.svg';
+import image from '../../images/assessmentBlock_image.svg';
 
 function AssessmentBlock() {
   // по клику на кнопку осуществится переход к анкете
@@ -17,24 +18,34 @@ function AssessmentBlock() {
               alt="иконка изображает лист бумаги и ручку"
               className="header__icon"
             />
-            <h3 className="header__text">
-              Оценка эффективности сотрудников
-            </h3>
+            <h3 className="header__text">Оценка эффективности сотрудников</h3>
           </div>
-          <button
-            className="header__button"
-            onClick={() => handleClick()}
-          >
+          <button className="header__button" onClick={() => handleClick()}>
             Провести анкетирование
           </button>
         </div>
         <div className="AssessmentBlock__filters">
-          <h3 className='filters__text'>Фильтры:</h3>
+          <h3 className="filters__text">Фильтры:</h3>
           <button className="filters__items filters__button">Оценить</button>
-          <div className="filters__items filters__window">Оценка поставлена</div>
-          <input type="text" placeholder='Поиск' className="filters__items filters__search"/>
+          <div className="filters__items filters__window">
+            Оценка поставлена
+          </div>
+          <input
+            type="text"
+            placeholder="Поиск"
+            className="filters__items filters__search"
+          />
           <form className="filters__items filters__calendar">Календарь</form>
         </div>
+        <img
+          src={image}
+          alt="картинка фона с изображением человека"
+          className="AssessmentBlock__immage"
+        />
+        <span className="AssessmentBlock__span">
+          <p className="">Список пока что пуст.</p>Новые карточки для оценки
+          сотрудников можете добавить с помощью кнопки «Провести анкетирование»
+        </span>
       </div>
     </section>
   );
