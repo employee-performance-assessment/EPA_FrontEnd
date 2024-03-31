75c1f421403fffab89b45756d7a27ed2252ba464
Дмитрий Русинов <dmitry-rusinov123@yandex.ru>
2024-03-31 15:10:42 +0300

    create command and personal state, Select component

--- src/pages/AnalyticsPage/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage/AnalyticsPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import SideMenu from '../../components/SideMenu/SideMenu.jsx';
@@ -5,41 +6,36 @@ import Checkbox from '../../components/Checkbox/Checkbox.jsx';
 import Select from '../../components/Select/Select.jsx';
 import SetStars from '../../components/SetStars/SetStars.js';
 
-// import flyMan from '../../images/fly-man.svg';
+import { getAllUsers } from '../../utils/mainApi.js';
+import data from './data.json';
+
+import flyMan from '../../images/fly-man.svg';
 
 import styles from './AnalyticsPage.module.scss';
 
 function AnalyticsPage() {
-  const data = [
-    {
-      year: 2024,
-      month: 'Январь',
-      rating: 5,
-      id: 1,
-    },
-    {
-      year: 2024,
-      month: 'Февраль',
-      rating: 4,
-      id: 2,
-    },
-    {
-      year: 2024,
-      month: 'Март',
-      rating: 3,
-      id: 3,
-    },
-  ];
   const isLoggedIn = useSelector((state) => state.isLoggedIn.isLoggedIn);
+  const [isPrivate, setIsPrivate] = useState(false);
+  const [isEstimate, setIsEstimate] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const { token } = JSON.parse(localStorage.getItem('token'));
+    getAllUsers(token).then((data) => setUsers(data));
+  }, []);
 
   const handleSubmitYear = () => {
-    console.log('query');
+    console.log('andleSubmitYear');
+  };
+
+  const handleSubmitUser = () => {
+    console.log('handleSubmitUser');
   };
 
   return isLoggedIn ? (
     <section className={styles.page}>
       <SideMenu />
-      <div className={styles.container}>
+      <div style={isEstimate ? { background: 'white' } : null} className={styles.container}>
         <header className={styles.header}>
           <div className={styles.analytic}>
             <span>Аналитика</span>
@@ -48,18 +44,40 @@ function AnalyticsPage() {
             shadow="none"
             labelLeft="Командная"
             labelRight="Индивидуальная"
+            isChecked={isPrivate}
+            setIsChecked={setIsPrivate}
           />
         </header>
         <div className={styles.filter_block}>
-          <Checkbox labelLeft="Оценки" labelRight="Дедлайны" />
-          <Select buttonText="Год" query={handleSubmitYear} typeSelect="list" />
+          <Checkbox
+            labelLeft="Оценки"
+            labelRight="Дедлайны"
+            isChecked={isEstimate}
+            setIsChecked={setIsEstimate}
+          />
+          {(isPrivate && !isEstimate) && <Select
+            typeSelect="users"
+            list={users}
+            buttonText="Исполнитель"
+            query={handleSubmitUser}
+            selectStyle={styles.user_select}
+            buttonStyle={styles.user_button}
+            listStyle={styles.users_ul}
+            optionStyle={styles.users_list}
+          />}
+          {!isEstimate && <Select
+            typeSelect="list"
+            list={['2023', '2024', '2025', '2026']}
+            buttonText="Год"
+            query={handleSubmitYear}
+            selectStyle={styles.select_year}
+            buttonStyle={styles.year_button}
+            listStyle={styles.year_ul}
+            optionStyle={styles.year_list}
+          />}
         </div>
-        <article className={styles.info_empty}>
-          {/* <div className={styles.img_block}>
-            <img className={styles.flyMan} src={flyMan} alt="Список пуст" />
-            <span>Данных для аналитики ещё нет.</span>
-          </div> */}
-          {data.map((item) => (
+        <article style={isEstimate ? { boxShadow: '0px 4px 20px 0px rgba(166, 166, 166, 0.4)' } : null} className={styles.info_block}>
+          {(data && !isEstimate) ? data.map((item) => (
             <div className={styles.rating_block} key={item.id}>
               <div className={styles.date}>
                 <span>
@@ -72,7 +90,10 @@ function AnalyticsPage() {
                 starIn={styles.starIn}
               />
             </div>
-          ))}
+          )) : <div className={styles.img_block}>
+            <img className={styles.flyMan} src={flyMan} alt="Список пуст" />
+            <span>Данных для аналитики ещё нет.</span>
+          </div>}
         </article>
       </div>
     </section>
