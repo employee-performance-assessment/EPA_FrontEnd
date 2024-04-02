b4f78d9483511468bb12d4db12797388fcd69b51
Irina Garmaeva <garmaeva-i@mail.ru>
2024-04-02 11:29:32 +0200

    style: edit userForm styles

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,23 +1,36 @@
 import './UserForm.scss';
 import CloseIcon from '../../images/closeIcon.png';
 
-function UserForm({ formTitle, handleSubmit, children, isValid, handleClosePopup }) {
+function UserForm({
+  formTitle,
+  handleSubmit,
+  children,
+  isValid,
+  handleClosePopup,
+}) {
   return (
     <div className="user-form__wrapper">
       <h3 className="user-form__title">{formTitle}</h3>
       <form className="user-form__form" onSubmit={handleSubmit}>
-        <fieldset className="user-form__fieldset">
-          {children}
-        </fieldset>
-        <button type='submit' className='user-form__submit-button' disabled={!isValid}>Подтвердить</button>
-      </form>
-      <button className="user-form__close-icon-block" type='button' onClick={handleClosePopup}>
-          <img
-            src={CloseIcon}
-            alt="иконка закрытия попапа"
-            className="user-form__closeIcon"
-          />
+        <fieldset className="user-form__fieldset">{children}</fieldset>
+        <button
+          type="submit"
+          className="user-form__submit-button"
+          disabled={!isValid}
+        >
+          Подтвердить
         </button>
+      </form>
+      <button
+        className="user-form__close-icon-block"
+        type="button"
+        onClick={handleClosePopup}
+      >
+        <span
+          style={{backgroundImage: `url(${CloseIcon})`}}
+          className="user-form__closeIcon"
+        />
+      </button>
     </div>
   );
 }
