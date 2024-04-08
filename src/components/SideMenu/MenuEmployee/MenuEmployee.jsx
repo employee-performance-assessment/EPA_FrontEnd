60d99cf2372b7fca43238c8bf694f6c1efaa72e5
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-04-08 20:47:04 +0500

    refactor(side-menu): refactor styles

--- src/components/SideMenu/MenuEmployee/MenuEmployee.jsx
+++ src/components/SideMenu/MenuEmployee/MenuEmployee.jsx
@@ -1,32 +1,63 @@
-import './MenuEmployee.scss';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ENDPOINT_ROUTES } from '../../../constants/constantsEndpointRoute';
+import '../MenuAdmin/MenuAdmin.scss';
 
 function MenuEmployee() {
+  const {
+    cardsEmployees,
+    taskCards,
+    ratingCards,
+    board,
+    estimate,
+    questionnaire,
+    analytics
+  } = ENDPOINT_ROUTES;
+
+  const { pathname } = useLocation();
+  const [isPersonalArea, setIsPersonalArea] = useState(false);
+  const [isBoard, setIsBoard] = useState(false);
+  const [isEstimate, setIsEstimate] = useState(false);
+  const [isAnalytics, setIsAnalytics] = useState(false);
+
+  useEffect(() => {
+    pathname.includes(cardsEmployees) ||
+      pathname.includes(taskCards) ||
+      pathname.includes(ratingCards) ?
+      setIsPersonalArea(true) : setIsPersonalArea(false);
+
+    pathname.includes(board) ?
+      setIsBoard(true) : setIsBoard(false);
+
+    pathname.includes(estimate) ||
+      pathname.includes(questionnaire) ?
+      setIsEstimate(true) : setIsEstimate(false);
+
+    pathname.includes(analytics) ?
+      setIsAnalytics(true) : setIsAnalytics(false);
+  }, [pathname]);
+
   return (
     <ul className="side-menu__list">
-      <Link
-        to="/#"
-        className="side-menu__item side-menu__item_personal"
-      >
-        <p className="side-menu__text">Личный кабинет</p>
+      <Link to={cardsEmployees} className="side-menu__item">
+        <div className={`side-menu__icon side-menu__icon-personal
+          ${isPersonalArea && 'side-menu__icon-personal_active'}`} />
+        <p className={`side-menu__text ${isPersonalArea && 'side-menu__text_active'}`}>Личный кабинет</p>
       </Link>
-      <Link
-        to="/#"
-        className="side-menu__item side-menu__item_kanban"
-      >
-        <p className="side-menu__text">Канбан доска</p>
+      <Link to={board} className="side-menu__item">
+        <div className={`side-menu__icon side-menu__icon-kanban
+          ${isBoard && 'side-menu__icon-kanban_active'}`} />
+        <p className={`side-menu__text ${isBoard && 'side-menu__text_active'}`}>Канбан доска</p>
       </Link>
-      <Link
-        to="/#"
-        className="side-menu__item side-menu__item_asses"
-      >
-        <p className="side-menu__text">Оценить коллегу</p>
+      <Link to={estimate} className="side-menu__item">
+        <div className={`side-menu__icon side-menu__icon-assessments
+          ${isEstimate && 'side-menu__icon-assessments_active'}`} />
+        <p className={`side-menu__text ${isEstimate && 'side-menu__text_active'}`}>Оценки коллегу</p>
       </Link>
-      <Link
-        to="/#"
-        className="side-menu__item side-menu__item_analytics"
-      >
-        <p className="side-menu__text"> Аналитика</p>
+      <Link to={analytics} className="side-menu__item">
+        <div className={`side-menu__icon side-menu__icon-analytics
+          ${isAnalytics && 'side-menu__icon-analytics_active'}`} />
+        <p className={`side-menu__text ${isAnalytics && 'side-menu__text_active'}`}>Аналитика</p>
       </Link>
     </ul>
   );
